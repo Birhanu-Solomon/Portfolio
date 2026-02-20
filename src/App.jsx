9412aa41b1496267c382aef7951bb8f082cbabab
Birhanu-Solomon <birhanu0329@gmail.com>
2026-02-20 19:04:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
 function App() {
   return <PortfolioClone />;
 }
-  )
-}
+  
+
 
 export default App
 
